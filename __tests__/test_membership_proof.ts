2b0dc5f9e8e2c2217c0399ad48016c54c7ae16cc
Olli Vanhoja <olli.vanhoja@gmail.com>
2019-10-16 23:58:49 +0300

    Add one more membership proof test

--- __tests__/test_membership_proof.ts
+++ __tests__/test_membership_proof.ts
@@ -37,4 +37,27 @@ describe('Membership proofs', () => {
 		expect(root).not.toBeNull();
 		expect(rootHash).toEqual(root && root.hash);
 	});
+
+	test('Prove that 5n is a member of the tree (boundary)', () => {
+		tree.insert(1n, Buffer.from('a'));
+		tree.insert(4n, Buffer.from('b'));
+		tree.insert(3n, Buffer.from('c'));
+		tree.insert(5n, Buffer.from('d'));
+
+		const proof = tree.membershipProof(5n);
+		const leftHash = genNodeHash(Buffer.from('a'), Buffer.from('c'));
+
+		expect(proof).toHaveLength(3);
+		proof.forEach((el: typeof proof[0]) => expect(el).toHaveLength(2));
+		expect(proof[0]).toEqual([Buffer.from('d'), 5n]);
+		expect(proof[1]).toEqual([Buffer.from('b'), 'L']);
+		expect(proof[2]).toEqual([leftHash, 'L']);
+
+		const rightHash = genNodeHash(Buffer.from('b'), Buffer.from('d'));
+		const rootHash = genNodeHash(leftHash, rightHash);
+		const root = tree.getRoot();
+
+		expect(root).not.toBeNull();
+		expect(rootHash).toEqual(root && root.hash);
+	});
 });
